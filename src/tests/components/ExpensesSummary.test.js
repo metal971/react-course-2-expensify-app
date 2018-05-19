739517f0f8d0d5b91ffbc2f759bf668050c2349c
Jack Pierre-Marie <jack.pierrem@gmail.com>
2018-05-19 17:11:07 +0200

    Add expense sumary to Dashboard

---
+++ src/tests/components/ExpensesSummary.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { ExpensesSummary } from '../../components/ExpensesSummary';
+import expenses from '../fixtures/expenses';
+
+test('should correctly render ExpensesSummary with 1 expense', () => {
+    const wrapper = shallow(<ExpensesSummary expenseCount={1} expensesTotal={2335} />); 
+    expect(wrapper).toMatchSnapshot();
+});
+
+
+
+test('should correctly render ExpensesSummary with multiple expenses', () => {
+    const wrapper = shallow(<ExpensesSummary expenseCount={23} expensesTotal={233534654646} />); 
+    expect(wrapper).toMatchSnapshot();
+});
