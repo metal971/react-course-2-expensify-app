a24a4e1a1f33d4e1181f63b3d65a1d6ea0301057
Jack Pierre-Marie <jack.pierrem@gmail.com>
2018-06-10 10:43:49 +0200

    Add Expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment';
-import { SingleDatePicker   } from 'react-dates';
+import { SingleDatePicker } from 'react-dates';
 
 
 export default class ExpenseForm extends React.Component {
@@ -15,8 +15,8 @@ export default class ExpenseForm extends React.Component {
             error: ''
         };
 
-    } 
-    
+    }
+
     onDescriptionChange = (e) => {
         const description = e.target.value
         this.setState(() => ({ description }));
@@ -26,72 +26,75 @@ export default class ExpenseForm extends React.Component {
         this.setState(() => ({ note }));
     };
     onAmountChange = (e) => {
-       const amount = e.target.value;
-       if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
-        this.setState(() => ({ amount })); 
-       }       
+        const amount = e.target.value;
+        if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }));
+        }
     };
     onDateChange = (createdAt) => {
         if (createdAt) {
             this.setState(() => ({ createdAt }));
-        }       
+        }
     };
     onFocusChange = ({ focused }) => {
         this.setState(() => ({ calendarFocused: focused }));
     };
     onSubmit = (e) => {
         e.preventDefault();
-        if (!this.state.description  || !this.state.amount) {           
+        if (!this.state.description || !this.state.amount) {
             const error = "error : Please provide description or amount ! ";
-            this.setState(() => ({error}));                
-        } else {             
-            this.setState(() => ({error: ''}));
+            this.setState(() => ({ error }));
+        } else {
+            this.setState(() => ({ error: '' }));
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount, 10) *100,
+                amount: parseFloat(this.state.amount, 10) * 100,
                 createdAt: this.state.createdAt.valueOf(),
                 note: this.state.note
-            })    
+            })
         }
-    }; 
-    
+    };
+
     render() {
         return (
-            <div>
-                 {this.state.error && <p>{this.state.error}</p>}           
-                <form onSubmit={this.onSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Description"
-                        autoFocus
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Amount"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}        
-                    />
-                    <SingleDatePicker
-                        date={this.state.createdAt}
-                        onDateChange={this.onDateChange}
-                        focused={this.state.calendarFocused}
-                        onFocusChange={this.onFocusChange}
-                        numberOfMonths={1}
-                        isOutsideRange={() => false}                   
-                    />
-                    <textarea
-                        placeholder="Add a note for your expense (optional) "
-                        value={this.state.note}
-                        onChange={this.onNoteChange}
-                    >
-                    </textarea>
-                    <button>
-                        Add Expense
-                    </button>     
-                </form>
-            </div>
+            <form className="form" onSubmit={this.onSubmit}>
+                {this.state.error && <p className="form__error">{this.state.error}</p>}
+                <input
+                    type="text"
+                    placeholder="Description"
+                    autoFocus
+                    className="text-input"
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input
+                    type="text"
+                    className="text-input"
+                    placeholder="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
+                <SingleDatePicker
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false}
+                />
+                <textarea
+                    className="textarea"
+                    placeholder="Add a note for your expense (optional) "
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                >
+                </textarea>
+                <div>
+                    <button className="button">
+                        Save Expense
+                    </button>
+                </div>
+            </form>
         )
     }
 }
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,19 +3,26 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-// regular component
 export const ExpenseList = (props) => (
-    <div>
-        <h1> Expense List</h1>
-        {
-            props.expenses.length === 0 ? (
-                <p>No expenses</p>
-            ) : (
-                    props.expenses.map((expense) => {
-                        return <ExpenseListItem key={expense.id} {...expense} />;
-                    })
-                )
-        }
+    <div className="content-container">
+        <div className="list-header">
+            <div className="show-for-mobile">Expenses</div>
+            <div className="show-for-others">Expense</div>
+            <div className="show-for-others">Amount</div>
+        </div>
+        <div className="list-body">
+            {
+                props.expenses.length === 0 ? (
+                    <div className="list-item list-item--message">
+                        <span>No expenses</span>
+                    </div>
+                ) : (
+                        props.expenses.map((expense) => {
+                            return <ExpenseListItem key={expense.id} {...expense} />;
+                        })
+                    )
+            }
+        </div>
     </div>
 );
 
